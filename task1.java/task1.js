65089da68414a5892f9da318b3f8b15bc71878ac
raneem-ibrahim <ibrahimraneem060@gmail.com>
2024-12-29 22:23:14 +0300

    task

---
+++ task1.java/task1.js
@@ -0,0 +1,21 @@
+let y=1000;
+ x=500; 
+ r=7;
+ e=9;
+ o=2;
+ a=150;
+ let u=1;
+ e=2;
+ q=3;
+ result=u**e * e**q;
+ let 
+document.write( "Cash flow ratio = " , y/x +"<br>");
+document.write("Net income =" , y-x +"<br>");
+document.write("Total assets =" , y+x +"<br>");
+document.write("Net income=" , y*x +"<br>");
+document.write("Average =" , (r+e+o)/3 +"<br>");
+document.write("Discount30% =", (30/100)*(a)-(a) +"<br>" );
+document.write("Age limit =" , (20>18 && 20<30) +("<br>"));
+document.write("Exponential =" , result +("<br>"));
+document.write("Remainder =" + 10 % 4);
+
---
+++ task1.java/task2.js
@@ -0,0 +1,23 @@
+num=100
+document.write (typeof num  +"<br>")
+numb=73.9
+document.write(typeof numb +"<br>");
+document.write(typeof NaN  +"<br>")
+document.write(typeof "water"  +"<br>")
+document.write(typeof false  +"<br>")
+document.write(typeof (9 != 11)  +"<br>")
+document.write("Orang" + "e"  +"<br>")
+document.write("Orange" - "s"  +"<br>") 
+document.write("4" + "8"  +"<br>")
+document.write("4" - "8"  +"<br>")
+document.write("4" - "8"  +"<br>")
+document.write("name" + 3  +"<br>")
+document.write("name" - 3  +"<br>")
+document.write(82 * "word"  +"<br>")
+document.write(1 + "hello"  +"<br>")
+document.write("hello" + 1  +"<br>")
+document.write(1 + true  +"<br>")
+document.write("hello" + true  +"<br>")
+document.write( typeof (Infinity)  +"<br>" )
+document.write(1 == '1'  +"<br>")
+document.write(1 === '1'  +"<br>")
