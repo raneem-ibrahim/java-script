7c4322113db30ca286abb23b9c66414f0175d54a
raneem-ibrahim <ibrahimraneem060@gmail.com>
2024-12-31 02:40:09 +0300

    task2

--- task2.java/task2.js
+++ task2.java/task2.js
@@ -9,8 +9,8 @@ if( score <= 50){
 console.log("you filed the test" )
 }
 // .........................................
-var x = "john" ;
-if( x == "john"  ){
+var name = "john" ;
+if( name == "john"  ){
    console.log("Hello,jhon")
 }
 // ...............................
@@ -31,9 +31,101 @@ switch(char){
         console.log("Its a litter")
 }
 // .............................................
-var list = [1,2,3]
-console.log (typeof list)
- if(){
+// var list = [1,2,3]
+//  switch(list){
+//     case "1":
+//      case "2":
+//      case "3":
+//         console.log("its aaray")
+//  }
+  list =[1,2,3]
+  if(list= "an array"){
+    console.log("Its array")
 
-}
+  }
+
 // ................................
+x =5;
+if(x>0){
+    console.log("y its apositive number")
+}
+// ........................................
+y = -5;
+if(y<0){
+    console.log("y is a negativ number")
+}
+// .....................................
+z= 9
+if(3*3 == z){
+    console.log(" z is a multiple of 3")
+}
+// ......................................................
+//  GPA =90;
+ 
+//  switch(GPA){
+//     case a:
+//         console.log("excellent");
+//     case b:
+//     console.log("very good");
+//     break;
+//     case c:
+//     console.log("good");
+//     break;
+//     case f:
+//     console.log("failed");
+//     break;
+// }
+// ......................................
+month = "january" ;
+switch(month){
+  case "December":
+  case "january":
+  case "february":
+    console.log("winter");
+    break;
+    case "march":
+     case "april":
+    case "may":
+   console.log("spring");
+    break;
+    case "june":
+     case "july":
+     case "august":
+        console.log("Summer");
+    break;
+    case "september":
+     case "october":
+    case "november":
+        console.log("Autumn");
+     break;
+}
+// ...........................................
+password ="mypassword123";
+switch(password){
+    case "mypassword123":
+        console.log("your password is strong")
+        break;
+    case "rewuy":  
+    console.log("your password is weak")
+    break;
+}
+// ...................................
+email ="admin@admin.com";
+password = "12345Admin";
+ if(email == "admin@admin.com" && password == "12345Admin"){
+   console.log("Welcome")
+}
+
+else if (email != "admin@admin.com") {
+    consle.log("incorrect email ")
+  
+}
+
+else if (password != "12345Admin" ){
+    consle.log("incorrect password")
+}
+ 
+// ...................................
+console.log(isNaN(123)); 
+console.log(isNaN("abc"));
+// console.log(isNumber(9) );
