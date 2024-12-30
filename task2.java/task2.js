fc7cf6488d7bce4bbc749aab99bd94a478017e78
raneem-ibrahim <ibrahimraneem060@gmail.com>
2024-12-30 23:44:00 +0300

    taskl2

---
+++ task2.java/task2.js
@@ -0,0 +1,39 @@
+var age =23;
+if( age > 18){
+   console.log("you ar an adult")
+}
+//.........................................
+
+var score = 45;
+if( score <= 50){
+console.log("you filed the test" )
+}
+// .........................................
+var x = "john" ;
+if( x == "john"  ){
+   console.log("Hello,jhon")
+}
+// ...............................
+var day ="monday" ;
+if (day != "sunday" && day != "saturday"){
+    console.log("Its a weekday")
+
+}
+// ........................................
+var num = 4
+if( 4 %2 == 0){
+console.log("The number is even")
+}
+// .............................................
+char ="a"
+switch(char){
+    case "a":
+        console.log("Its a litter")
+}
+// .............................................
+var list = [1,2,3]
+console.log (typeof list)
+ if(){
+
+}
+// ................................
